a6b07613ca6dc6921e8cbb3bd2a73dabd2f56489
Dionisis Pettas <dspedi@gmail.com>
2019-11-23 16:16:56 +0200

    Started to build the Phi class for the Basis Funtions

--- include/BasisFunctions/Phi.hpp
+++ include/BasisFunctions/Phi.hpp
@@ -1,22 +1,83 @@
 #ifndef PHI_H
 #define PHI_H
 
-class Phi
-{
-	private: 
-	double val; 
-	double derivative; 
+#include <cmath>
+
+
+
+
+class Phi{
+
+	private:
+		double (*f )(double) = NULL  ;
+		double (*df)(double) = NULL  ;
+		double eps           = 1.e-8 ;
+		double val           = std::nan("");
 
 	public:
+		Phi(){};
+		Phi( double (*f_) (double)                         );
+		Phi( double (*f_) (double), double value           );
+		Phi( double (*f_) (double), double (*df_) (double) );
 
+		//<><><><><><><><><><><><><><><><><><><><><><><><><><>
+		// Setters 
+		//<><><><><><><><><><><><><><><><><><><><><><><><><><>
+		void   set_epsilon ( double eps_ ); 
+		double set_value   ( double val_ );
 
+		//<><><><><><><><><><><><><><><><><><><><><><><><><><>
+		// Getters
+		//<><><><><><><><><><><><><><><><><><><><><><><><><><>
 
-	double get_val()        const {return val;}
-	double get_derivative() const {return derivative;}	
-	double ddx()            const {return derivative;}
+		double get_value  ();
+		
+		//<><><><><><><><><><><><><><><><><><><><><><><><><><>
+		// Methods
+		//<><><><><><><><><><><><><><><><><><><><><><><><><><>
 
+		double evaluate             (double val_);
+		double evalueate            ()           ;
+		double derivative           (double val_);
+		double numerical_derivative (double val_);
 
 };
+
+
+
+Phi::Phi( double (*f_)  (double) ) { f = f_; df = NULL;}
+
+Phi::Phi( double (*f_)  (double) ,
+				  double value ) { f = f_; val = value; }
+
+Phi::Phi( double (*f_)  (double),
+					double (*df_) (double) ) { f = f_; df = df_ ;}
+
+
+
+
+
+
+double Phi::get_value () {return val;}
+
+
+double Phi::evaluate (double val_){return  f(val_);}
+//double Phi::evaluate ()           {return  f(val );}
+
+
+double Phi::derivative(double val_)
+{
+	if   (df != NULL) { return df(val_)    ;}
+	else              { return std::nan("");}
+}
+
+double Phi::numerical_derivative(double val_)
+{
+	return (f(val_ + eps)- f(val_))/eps;
+}
+
+
+
 #endif
 
 
--- main.cpp
+++ main.cpp
@@ -8,56 +8,16 @@
 #include "BasisFunctions/Phi.hpp"
 
 
-class Test{
-
-	private:
-	double (*func)(double) = NULL;
-
-
-	public:
-		Test(){};
-		Test( double (*bf_) (double) ) { func = bf_;   }
-
-		double set_value  ( double val); // calculate the function and derivative
-		double evaluate   ( double val);
-		double derivative ( double val);
-
-};
-
-
-double Test::derivative( double val){
-	return (func(val+1.e-12) - func(val))/1.e-12;
-}
-
-double Test::evaluate( double val) {return func(val);}
-
-
-
-double function_0( double x);
-
-
 
 
 int main()
 {
+	Phi phi_0 ( [](double x) {return x*x; }, 5.0 );
 
-	Test phi  ( function_0 ) ; 
-	Test phi_ ([](double x ) { return x;} );
-
-	std::cout << phi.evaluate  (5.0) << std::endl;	
-	std::cout << phi.derivative(5.0) << std::endl;	
-
-
-	std::cout << phi_.evaluate  (5.0) << std::endl;	
-	std::cout << phi_.derivative(5.0) << std::endl;	
-
-
+	std::cout<< phi_0.evaluate(2.0) << std::endl;
 
 	return 0;
 }
-
-
-double function_0( double x)  { return x*x*x;}
 
 
 
